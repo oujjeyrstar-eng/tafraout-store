f9ade92f6a73cae690e2d6ee485ad2615a31bd20
oujjeyrstar-eng <oujjeyrstar@gmail.com>
2026-07-16 22:32:34 +0100

    Delete functions directory

--- functions/api/logout.js
+++
@@ -1,8 +0,0 @@
-import { requireAdminCsrf, clearSessionCookie } from '../_lib/auth.js';
-import { json } from '../_lib/utils.js';
-
-export async function onRequestPost({ request, env }) {
-  const auth = await requireAdminCsrf(request, env, { csrfRequired: false });
-  // حتى لو الجلسة سالات، نصفيو الكوكي فكل الحالات
-  return json({ ok: true }, 200, { 'Set-Cookie': clearSessionCookie() });
-}
